5cd096eef56072b359acec0cb792b0c351eb36af
Daniel Dean <daniel.dean414@gmail.com>
2019-12-02 11:19:20 -0700

    Added steps to load country name data

--- initial_country_tally.R
+++ initial_country_tally.R
@@ -1,14 +1,19 @@
 #comment out once s1 is loaded
-source("s1_download.R")
+#source("s1_download.R")
+
+library(rworldmap)
+data("countryExData")
+data("countryRegions")
+data("countrySynonyms")
 
 
 jeopardy_s1 %>%
   filter(str_detect(string = answer, pattern = paste(countrySynonyms$name1, collapse = "|") ) | str_detect(string = question, pattern = paste(countrySynonyms$name1, collapse = "|") )) %>%
   mutate(country = str_extract(string = answer, pattern = paste(countrySynonyms$name1, collapse = "|") )) %>%
   mutate(country2 = str_extract(string = question, pattern = paste(countrySynonyms$name1, collapse = "|") )) %>%
-  group_by(country) %>%
+  group_by(daily_double,country) %>%
   add_tally(name = "country_1") %>%
   ungroup %>%
-  group_by(country2) %>%
+  group_by(daily_double, country2) %>%
   add_tally(name = "country_2")
 
