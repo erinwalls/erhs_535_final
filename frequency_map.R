b697da1054d40b14b7ac5120966af66c02d65403
Daniel Dean <daniel.dean414@gmail.com>
2019-12-04 12:05:55 -0700

    Added a quick demonstration map of total S1 frequency using `rworldmaps`; apparently 2 of the codes don't match, and the default color scaling seems really odd. It might be interesting seeing if e.g. Tigris or another package accepts ISO3 codes

--- frequency_map.R
+++ frequency_map.R
@@ -1,3 +1,16 @@
 #############################
-R Script for frequency map
+#R Script for frequency map
 #############################
+
+#Adding a quick demonstration for S1 using rworldmap (obviously could stand cleaning-up, use a different package, etc.):
+
+
+source("country_dataframes.R")
+library(tidyverse)
+library(rworldmap)
+
+iso3_tally %>%
+  mutate(iso3 = toupper(iso3)) %>%
+  rename(ISO3V10 = iso3) %>%
+  joinCountryData2Map(joinCode = "ISO3", nameCountryColumn = "ISOV10") %>%
+  mapCountryData(nameColumnToPlot = "count")
