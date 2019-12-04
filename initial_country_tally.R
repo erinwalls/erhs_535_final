c133f32664eb14ba70c1fff849b7d909b4332670
Daniel Dean <daniel.dean414@gmail.com>
2019-12-04 10:40:10 -0700

    Hopefully updating import; will plan on cleaning up as well

--- initial_country_tally.R
+++ initial_country_tally.R
@@ -9,13 +9,6 @@ data("countryExData")
 data("countryRegions")
 data("countrySynonyms")
 
-#Adding demonyms: 
-
-demonyms <- read_csv("https://raw.githubusercontent.com/knowitall/chunkedextractor/master/src/main/resources/edu/knowitall/chunkedextractor/demonyms.csv") %>%
-  dplyr::rename(demonym = Aalborgenser, country = Aalborg) #%>%
-  filter(country %in% countrySynonyms$name1)
-
-  
   #Noticed my current list seems spotty (e.g. "Korean" isn't included); trying to find my older one
   
   
@@ -61,18 +54,23 @@ demonyms <- read_csv("https://raw.githubusercontent.com/knowitall/chunkedextract
   
 
 jeopardy_s1_tallies_1 <- jeopardy_s1 %>%
-  filter(str_detect(string = answer, pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[[:punct:]|[:space:]|s]")) |
-           str_detect(string = question, pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[[:punct:]|[:space:]|s]"))) %>%
-  mutate(country_a = gsub('[[:punct:] ]+','', str_extract(string = answer,
-                                 pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[[:punct:]|[:space:]|s]")))) %>%
-  mutate(country_q = gsub('[[:punct:] ]+','', str_extract(string = question,
-                                 pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[[:punct:]|[:space:]|s]"))))
+  filter(str_detect(string = answer, pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[ [:punct:] | [:blank:] | [:space:] ]")) |
+           str_detect(string = question, pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[ [:punct:] | [:blank:] | [:space:] ]"))) %>%
+  #mutate(country_a = -gsub('[[:punct:] ]','', str_extract_all(string = answer,
+                                 #pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[ [:punct:] | [:blank:] | [:space:] ]")))) %>%
+  #mutate(country_q = gsub('[[:punct:] ]','', str_extract_all(string = question,
+      #                           pattern = paste0("(",paste(country_names_full$names, collapse = "|"),")","[ [:punct:] | [:blank:] | [:space:] ]"))))
+  mutate(country_a = str_extract_all(string = answer,
+    pattern = paste0("(",
+                     paste(country_names_full$names, collapse = "|"),
+                     ")",
+                     "[ [:punct:] | [:blank:] | [:space:] ]")),
+          country_q= str_extract_all(string = question,
+                                           pattern = paste0("(",
+                                                            paste(country_names_full$names, collapse = "|"),
+                                                            ")",
+                                                            "[ [:punct:] | [:blank:] | [:space:] ]")))
   
-#Note: this doesn't detect all synonyms;
-jeopardy_s1 %>% 
-  mutate(US = str_detect(answer, "America|US|United States|United States of America|USA")) %>% 
-  filter(US == TRUE)
-
 jeopardy_s1_tallies_1 %>%
   select(country_a, country_q) %>%
   pivot_longer(c(country_q, country_a), names_to = "prompt", values_to = "country") %>%
