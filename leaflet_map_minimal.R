482f106b549787e854261c595160d269ed10300c
Daniel Dean <daniel.dean414@gmail.com>
2019-12-05 13:14:27 -0700

    Adding minimal example of leaflet map (hopefully caught all dependencies)

---
+++ leaflet_map_minimal.R
@@ -0,0 +1,26 @@
+
+library(leaflet)
+library(tidyverse)
+source("country_dataframes.R") #To get country_all_isoobject; can comment this pout if already loaded
+
+test_geom_full <- country_all_iso %>%
+  group_by(iso3) %>%
+  add_tally(name = "count") %>%
+  ungroup() %>%
+  mutate(iso3 = toupper(iso3),
+         iso3 = as.factor(iso3)) %>%
+  rename(ISO3 = iso3) %>%
+  full_join(test_geom) %>%
+  mutate(ISO3 = as.factor(ISO3)) %>%
+  clean_names() %>%
+  st_as_sf
+
+
+pal <- colorNumeric(
+  palette = 'Dark2',
+  domain = test_geom_full$count
+)
+
+leaflet(test_geom_full) %>%
+  addTiles() %>%
+  addPolygons(color = ~pal(count))
