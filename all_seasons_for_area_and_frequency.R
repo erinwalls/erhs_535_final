56dd1ee16559d3ecafacf1c469b77b23f2cfefef
ChaoyuZhai <Chaoyu.Zhai@colostate.edu>
2019-12-11 11:30:35 -0700

    Chaoyu added

--- all_seasons_for_area_and_frequency.R
+++ all_seasons_for_area_and_frequency.R
@@ -41,7 +41,7 @@ library(dlnm)
 jeopardy_all_season <- jeopardy_all_season  %>%
   mutate(Ratio = count / landarea)
 
-view(jeopardy_all_season) 
+str(jeopardy_all_season) 
 
 mod_2 <- lm(count ~ landarea, data = jeopardy_all_season  )
 mod_2
@@ -57,45 +57,3 @@ augment(mod_2) %>%
   geom_line(aes(x = landarea, y = .fitted), color = "red", size = 2) + 
   theme_classic()  
 
-
-
-
-#Loading demonyms
-
-webpage <- read_html("https://en.wikipedia.org/wiki/List_of_adjectival_and_demonymic_forms_for_countries_and_nations")
-
-table <- webpage %>%
-  html_nodes("table") %>%
-  html_table(header=F)
-table <- table[[1]]
-
-
-names(table) = table[1,]
-table <- table %>%
-  slice(-1) %>%
-  clean_names()
-
-#Converting country synoyms to full list
-
-
-demonym_table <- table %>%
-  as.tibble() %>%
-  mutate(country_entity_name = str_replace(country_entity_name, "\\[.\\]", ""),
-         adjectivals = str_replace(adjectivals, "\\[.\\]", ""),
-         demonyms = str_replace(demonyms, "\\[.\\]", "")) %>%
-  #
-  separate_rows(adjectivals, sep = ",\\s|/|\\sor\\s") %>%
-  separate_rows(demonyms, sep = ",\\s|/|\\sor\\s")
-
-
-
-countrySynonyms_full <- countrySynonyms %>%
-  pivot_longer(name1:name8, names_to = "name", values_to = "country") %>%
-  filter(!is.na(country) & country != "") 
-
-country_names_full <- countrySynonyms_full %>%
-  select(-c(name, ID)) %>%
-  left_join(demonym_table, by = c("country" = "country_entity_name")) %>%
-  pivot_longer(country:demonyms, names_to = "name_type", values_to = "names") %>%
-  drop_na()%>%
-  clean_names()
