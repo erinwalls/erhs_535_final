b83deb92c8e9ece5af7addee3b062b2f482c9301
ChaoyuZhai <Chaoyu.Zhai@colostate.edu>
2019-12-11 15:00:23 -0700

    Chaoyu Added

--- all_seasons_for_area_and_frequency.R
+++ all_seasons_for_area_and_frequency.R
@@ -14,6 +14,10 @@ library(lme4)
 library(lmerTest)
 library(car)
 library(emmeans)
+library(ggrepel)
+library(readr)
+library(ggplot2)
+library(dlnm)
 data("countryExData")
 data("countryRegions")
 data("countrySynonyms")
@@ -33,29 +37,45 @@ jeopardy_all_season  <-  country_all_iso_all%>%
          iso3 = as.factor(iso3)) %>%
   rename(ISO3V10 = iso3) %>%
   full_join(countryExData) %>%
-  filter(!is.na(count))
+  filter(!is.na(count)) %>%
+  mutate(Ratio = count / landarea)
 
-
-jeopardy_all_season    %>%
-  mutate(Ratio = count / landarea) %>%
+all_season_worldmap <- jeopardy_all_season %>%
   joinCountryData2Map(joinCode = "ISO3", nameCountryColumn = "ISO3V10") %>%
   mapCountryData(nameColumnToPlot = "Ratio") 
 
 
-library(dlnm)
-
-
-jeopardy_all_season <- jeopardy_all_season  %>%
+all_season_simple <- jeopardy_all_season  %>%
   mutate(Ratio = count / landarea) %>%
   mutate(air_date = ymd(air_date))%>%
   separate(air_date, sep="-", into = c("year", "month", "day")) %>%
   mutate(season = year) %>%
-  mutate(season = as_factor(x = season))
+  mutate(season = as_factor(x = season))%>%
+  select(count,Ratio,season,landarea,ISO3V10,GDP_capita.MRYA)%>%
+  rename(country=ISO3V10) %>%
+  mutate(country = as_factor(x=country)) %>%
+  filter (!is.na(country))
 
-str(jeopardy_all_season)
+view(all_season_simple) 
 
-mod_2 <- lmer(count ~ landarea*season +(1|GDP_capita.MRYA), data = jeopardy_all_season)
-Anova(mod_2, type= 3)
+all_season_unique <-all_season_simple %>%
+  unique() %>%
+  filter (!is.na(landarea)) %>%
+  filter (!is.na(GDP_capita.MRYA)) 
+
+head(all_season_unique) 
+
+str(all_season_unique)
+
+write.csv(all_season_unique, file = "all_season_unique.csv")
+
+view(all_season_unique)
+
+
+
+mod_2 <- lm(Ratio ~ season, data = all_season_unique)
+Anova(mod_2, type = 3)
+
 
 
 library(broom)
@@ -63,9 +83,18 @@ tidy(mod_2)
 
 glance(mod_2)
 
-augment(mod_2) %>%
-  ggplot(aes(x = landarea, y = count)) + 
-  geom_point(size = 0.8, alpha = 0.5, col = "gray") + 
-  geom_line(aes(x = landarea, y = .fitted), color = "red", size = 2) + 
-  theme_classic()  
+map_count_season_landarea <- jeopardy_all_season_simple%>%
+  ggplot() + 
+  geom_point(aes(x = season, y = count, color = landarea),size=0.5) + 
+  geom_text_repel(aes(x = season, y = count, color=landarea, label=country),
+                  force = 0.5, direction = c("both"),size=2)+
+  theme_classic(base_size = 7) +
+  theme(plot.title = element_text(size = 7, face = "bold"),
+        axis.title = element_text(size = 7), legend.position = "bottom") +
+  labs(x = "Seasons",y = expression('Frequencey'),
+       color = "Landarea") 
 
+
+geom_line(aes(x = season, y = count, color = landarea, group=country), size = 0.5) 
+  
+map_count_season_landarea 
